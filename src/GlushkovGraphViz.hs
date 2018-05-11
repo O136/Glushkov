1328902f31be4e0d84bd74fd815ca681778d2327
Oleg Patraschku <oleg.patraschku@gmx.de>
2018-05-11 09:22:55 +0000

    moved to tests

--- src/GlushkovGraphViz.hs
+++ src/GlushkovGraphViz.hs
@@ -51,13 +51,3 @@ graphVizAutomaton t word =
      concatMap ((++ "\n") . trans2Str) trans ++
      "start->" ++ state2Str [initS] ++ "\n}"
 
---(a|b)*a(a|b)
-regEx :: RegT
-regEx =
-  Concat
-    ( Star (Or (Letter (0, 'a'), Letter (1, 'b')))
-    , Concat (Letter (2, 'a'), Or (Letter (3, 'a'), Letter (4, 'b'))))
-
---(a)*
-regEx2 :: RegT
-regEx2 = Star (Letter (0, 'a'))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+import Glushkov
+import GlushkovGraphViz
+
+--(a|b)*a(a|b)
+regEx :: RegT
+regEx =
+  Concat
+    ( Star (Or (Letter (0, 'a'), Letter (1, 'b')))
+    , Concat (Letter (2, 'a'), Or (Letter (3, 'a'), Letter (4, 'b'))))
+
+--(a)*
+regEx2 :: RegT
+regEx2 = Star (Letter (0, 'a'))
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
